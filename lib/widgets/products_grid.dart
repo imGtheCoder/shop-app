b149ab81ddbf334ea7f8a7a77323d1a98a14f1c8
imGtheCoder <stiopimanol34@gmail.com>
2023-01-21 19:51:47 +0200

    Transferring data through providers

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -20,10 +20,13 @@ class ProductsGrid extends StatelessWidget {
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
         ),
-        itemBuilder: (context, i) => ProductItem(
-              products[i].id,
-              products[i].title,
-              products[i].imageUrl,
-            ));
+        itemBuilder: (context, i) => ChangeNotifierProvider(
+          create: (c) => products[i],
+          child: ProductItem(
+                // products[i].id,
+                // products[i].title,
+                // products[i].imageUrl,
+              ),
+        ));
   }
 }
