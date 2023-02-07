5f03a2505b66da95235b593fc4d64e4aff06c65c
imGtheCoder <stiopimanol34@gmail.com>
2023-02-07 18:46:27 +0200

    animated the orders

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -18,50 +18,54 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(children: [
-        ListTile(
-          title: Text('\$${widget.order.amount}'),
-          subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
-          trailing: IconButton(
-            icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-            onPressed: () {
-              setState(() {
-                _expanded = !_expanded;
-              });
-            },
-          ),
-        ),
-        // ignore: sdk_version_ui_as_code
-        if (_expanded)
-          Container(
-            padding:const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-            height: min(widget.order.products.length * 20.0 + 10.0, 100),
-            child: ListView(
-              children: [
-                ...widget.order.products
-                    .map((product) => Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              product.title,
-                              style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              '${product.quantity}x \$${product.price}',
-                              style:
-                                  TextStyle(fontSize: 18, color: Colors.grey),
-                            ),
-                          ],
-                        ))
-                    .toList(),
-              ],
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 200),
+      height: _expanded ? min(widget.order.products.length * 20.0 + 110.0, 200) : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(children: [
+          ListTile(
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
           ),
-      ]),
+          // ignore: sdk_version_ui_as_code
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              padding:const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: _expanded ? min(widget.order.products.length * 20.0 + 10.0, 100) : 0,
+              child: ListView(
+                children: 
+                  widget.order.products
+                      .map((product) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                product.title,
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                '${product.quantity}x \$${product.price}',
+                                style:
+                                    TextStyle(fontSize: 18, color: Colors.grey),
+                              ),
+                            ],
+                          ))
+                      .toList(),
+                
+              ),
+            ),
+        ]),
+      ),
     );
   }
 }
